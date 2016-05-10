780981a9e6555162c5fedd63d0eb5a884cb2e5a2
jmkiley <jordankiley@gmail.com>
2016-05-10 13:25:23 -0400

    Done.

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -24,5 +24,31 @@
  * Write your method definitions here.
  
  */
+- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    NSMutableArray *arrayCopy = [ NSMutableArray new ] ;
+    for ( NSUInteger i = 0 ; i < [ array count ]; i++ ) {
+        [ arrayCopy addObject:array[i]];
+        
+    }
+    [ arrayCopy addObject:string ] ;
+    return arrayCopy ;
+}
+
+- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    NSUInteger countOfUppercaseStringsInArray = 0 ;
+    for (NSUInteger i = 0 ; i < [ array count ]; i++ ) {
+        if ([ array[i] isEqualToString:[ array[i] uppercaseString ] ]) {
+            countOfUppercaseStringsInArray++ ;
+            
+        }
+        NSLog( @"%lu", countOfUppercaseStringsInArray ) ;
+    }
+    return countOfUppercaseStringsInArray;
+}
+
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    [ array removeAllObjects ];
+    
+}
 
 @end
